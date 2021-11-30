3638f122d43e6304ab943449e27a731ce2eb8554
SaifulKIslam <saifulislam123198@gmail.com>
2021-11-30 11:19:31 +0000

    update

--- service1/application/route.py
+++ service1/application/route.py
@@ -1,14 +1,22 @@
+from flask import Flask, render_template, request
+import requests
+from application import app, db
+from application.models import car_generator
+from sqlalchemy import desc
+
+
 @app.route('/', methods=['GET','POST'])
 def index():
     #get the make of the car 
-    make = requests.get("")
+    make = requests.get(""http://encounters_server2:5000/make"")
     #get the shape
-    shape = requests.get("")
+    shape = requests.get("http://encounters_server3:5000/shape")
+    
 
     model_result = str(make.text) + " " + str(shape.text) 
    
    #post to service 4
-    model = requests.post("", data=model_result)
+    model = requests.post("http://encounters_server4:5000/model", data=model_result)
     
     last_3_cars = car_generator.query.order_by(desc(car_generator.id)).limit(3).all()
     db.session.add(
