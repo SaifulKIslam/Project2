67bdb28b5d206b4c7b5b2e7acbd4f8304ffd5aae
SaifulKIslam <saifulislam123198@gmail.com>
2021-11-30 12:12:58 +0000

    service

--- service1/application/route.py
+++ service1/application/route.py
@@ -8,15 +8,15 @@ from sqlalchemy import desc
 @app.route('/', methods=['GET','POST'])
 def index():
     #get the make of the car 
-    make = requests.get(""http://encounters_server2:5000/make"")
+    make = requests.get("/make")
     #get the shape
-    shape = requests.get("http://encounters_server3:5000/shape")
+    shape = requests.get("/shape")
     
 
     model_result = str(make.text) + " " + str(shape.text) 
    
    #post to service 4
-    model = requests.post("http://encounters_server4:5000/model", data=model_result)
+    model = requests.post("/model", data=model_result)
     
     last_3_cars = car_generator.query.order_by(desc(car_generator.id)).limit(3).all()
     db.session.add(
@@ -32,6 +32,6 @@ def index():
     make = make.text, 
     shape=shape.text, model = model.text, 
     pmodel_result=model_result, 
-    last_3_car=last_3_cars)
+    last_3_cars=last_3_cars)
 
 
